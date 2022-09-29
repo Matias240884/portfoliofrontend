e48d1288154a53308fa44268779c4ea9c0a4e6a9
Matias <matiasramunno84@gmail.com>
2022-09-29 06:52:13 -0300

    Primer componente header

--- task-list/src/app/app.module.ts
+++ task-list/src/app/app.module.ts
@@ -3,13 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
-import { ButtonComponent } from './components/button/button.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    ButtonComponent
+    HeaderComponent
   ],
   imports: [
     BrowserModule
--- task-list/src/app/components/header/header.component.ts
+++ task-list/src/app/components/header/header.component.ts
@@ -7,15 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  title: string = 'Task List';
-
   constructor() { }
 
   ngOnInit(): void {
-  }
-
-  toggleAddTask(){
-    console.log("toggleAddTask!");
   }
 
 }
