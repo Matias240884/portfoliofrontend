be9b4af58873ef03d6923ca3c1cb49bfe8b2e5ed
Matias <matiasramunno84@gmail.com>
2022-09-29 11:04:41 -0300

    Primer boton

--- task-list/src/app/components/header/header.component.ts
+++ task-list/src/app/components/header/header.component.ts
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  title:string = 'Task List';
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  toggleAddTask(){
+    console.log("toggleAddTask!"); 
   }
 
 }
