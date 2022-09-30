7442bf5e8cad725db1344579bc2fb264c88ec2c8
Matias <matiasramunno84@gmail.com>
2022-09-30 07:05:33 -0300

    Agregar mock de tareas y el interface de las mismas

---
+++ task-list/src/app/Task.ts
@@ -0,0 +1,6 @@
+export interface Task {
+    id?: number; //podria no venir
+    text: string;
+    day: string;
+    reminder: boolean;
+}
--- task-list/src/app/components/header/header.component.ts
+++ task-list/src/app/components/header/header.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
@@ -7,15 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  title:string = 'Task List';
+  title: string = 'Task List';
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
-  toggleAddTask(){
-    console.log("toggleAddTask!"); 
+  toggleAddTask() {
+    console.log("toggleAddTask!");
   }
 
 }
